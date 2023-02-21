234305c3607f2d43d1be39057822a1e32fb37062
VictorBrazOficial <vic_braz@outlook.com>
2023-02-21 15:03:12 -0300

    Adicionar arquivos de projeto.

---
+++ Exercicios/Iniciantes/Salario.cs
@@ -0,0 +1,31 @@
+﻿using System.Globalization;
+
+namespace Exercicios.Iniciantes
+{
+    public class Salario
+    {
+        public int IdFuncionario { get; set; }
+        public int HorasTrabalhadas { get; set; }
+        public decimal SalarioHora { get; set; }
+        public decimal SalarioFuncionario { get; set; }
+
+        public void EntradaDados()
+        {
+            IdFuncionario = int.Parse(Console.ReadLine());
+            HorasTrabalhadas = int.Parse(Console.ReadLine());
+            SalarioHora = decimal.Parse(Console.ReadLine());
+        }
+
+        public decimal Calcula(int horasTrabalhadas, decimal salarioHora)
+        {
+            SalarioFuncionario = horasTrabalhadas * salarioHora;
+
+            return SalarioFuncionario;
+        }
+
+        public void Print()
+        {
+           Console.WriteLine($"NUMBER = {IdFuncionario}\nSALARY = U$ {SalarioFuncionario.ToString("n2", new CultureInfo("en-USA"))}");
+        }
+    }
+}
---
+++ Exercicios/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using Exercicios.Iniciantes;
+
+var salario = new Salario();
+salario.EntradaDados();
+salario.Calcula(salario.HorasTrabalhadas, salario.SalarioHora);
+salario.Print();
