b9f1b9c07dba12066b68c5f03a443d5a2d3226d8
VictorBrazOficial <vic_braz@outlook.com>
2023-02-25 14:05:27 -0300

    CalculoSimples: Adicionando Exception

---
+++ Exercicios/Exception/EntradaException.cs
@@ -0,0 +1,8 @@
+﻿namespace Exercicios.Exception
+{
+    public class EntradaException : SystemException
+    {
+        public EntradaException() : base("O terceiro valor deve conter 2 casas decimais.")
+        { }
+    }
+}
